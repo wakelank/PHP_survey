d891cac23b0a3580ac7ef8950e0de7673ae6332e
wakelank <wakelank@gmail.com>
2014-09-10 15:56:58 -0400

    thankyou page accepts data

--- thankyou.php
+++ thankyou.php
@@ -1,1 +1,16 @@
-Great! Thanks for responding.
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="stylesheets/styles.css">
+  <title>
+    Thank you!
+  </title>
+</head>
+<body>
+
+  <?php
+  echo "Great! Thanks, " . $_POST['firstName'] . " for responding to our survey.<br><h1>aikaaika</h1>aikaikaiaikaika"
+  ?>
+
+</body>
+</html>
